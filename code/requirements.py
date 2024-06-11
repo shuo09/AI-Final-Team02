1c1930f704918d78e0e7c2aaa5ca657450ca575d
shuo09 <167032299+shuo09@users.noreply.github.com>
2024-06-11 17:55:32 +0800

    Create requirements.py

---
+++ code/requirements.py
@@ -0,0 +1,19 @@
+# import module
+import os
+import glob
+import random
+from datetime import datetime
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
+from torch import optim
+from torch.autograd import Variable
+from torch.utils.data import Dataset, DataLoader
+
+import matplotlib.pyplot as plt
+import numpy as np
+import logging
+from tqdm import tqdm
