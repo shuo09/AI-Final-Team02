e091f896344d64d7ccdc8bd27431c6d6623ec886
shuo09 <167032299+shuo09@users.noreply.github.com>
2024-06-11 17:53:20 +0800

    Update and rename python.py to Generator.py

---
+++ code/Generator.py
@@ -0,0 +1,40 @@
+# Generator
+
+class Generator(nn.Module):
+    """
+    Input shape: (batch, in_dim)
+    Output shape: (batch, 3, 64, 64)
+    """
+    def __init__(self, in_dim, feature_dim=64):
+        super().__init__()
+    
+        #input: (batch, 100)
+        self.l1 = nn.Sequential(
+            nn.Linear(in_dim, feature_dim * 8 * 4 * 4, bias=False),
+            nn.BatchNorm1d(feature_dim * 8 * 4 * 4),
+            nn.ReLU()
+        )
+        self.l2 = nn.Sequential(
+            self.dconv_bn_relu(feature_dim * 8, feature_dim * 4),               #(batch, feature_dim * 16, 8, 8)     
+            self.dconv_bn_relu(feature_dim * 4, feature_dim * 2),               #(batch, feature_dim * 16, 16, 16)     
+            self.dconv_bn_relu(feature_dim * 2, feature_dim),                   #(batch, feature_dim * 16, 32, 32)     
+        )
+        self.l3 = nn.Sequential(
+            nn.ConvTranspose2d(feature_dim, 3, kernel_size=5, stride=2,
+                               padding=2, output_padding=1, bias=False),
+            nn.Tanh()   
+        )
+        self.apply(weights_init)
+    def dconv_bn_relu(self, in_dim, out_dim):
+        return nn.Sequential(
+            nn.ConvTranspose2d(in_dim, out_dim, kernel_size=5, stride=2,
+                               padding=2, output_padding=1, bias=False),        #double height and width
+            nn.BatchNorm2d(out_dim),
+            nn.ReLU(True)
+        )
+    def forward(self, x):
+        y = self.l1(x)
+        y = y.view(y.size(0), -1, 4, 4)
+        y = self.l2(y)
+        y = self.l3(y)
+        return y
